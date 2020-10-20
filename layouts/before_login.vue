907e615c147be80091bbb4679dcdb6c7572d1759
Eduarda Ramos <2171347@my.ipleiria.pt>
2020-10-20 14:01:48 +0100

    1º commit

---
+++ layouts/before_login.vue
@@ -0,0 +1,52 @@
+<template>
+ <v-app dark>
+
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      color="cyan darken-1"
+    >
+
+      <v-spacer />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+
+  </v-app>
+</template>
+
+<script>
+export default {
+name: "before_login",
+ data () {
+    return {
+      clipped: true,
+      drawer: false,
+      fixed: true,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Project+'
+    }
+  }
+}
+</script>
+
---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <div class="text-center">
+        <logo />
+        <vuetify-logo />
+      </div>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to the Vuetify + Nuxt.js template
+        </v-card-title>
+        <v-card-text>
+          {{this.projetos}}
+        </v-card-text>
+        <v-card-actions>
+
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+
+export default {
+  layout: "before_login",
+  components: {
+    Logo,
+    VuetifyLogo
+  },
+  data:function (){
+    return {
+      projetos: [],
+    }
+  },
+  methods:{
+    getProjetos () {
+
+      this.$axios.$get('/api/projetos/all')
+        .then((response) => {
+          this.projetos = response;
+        })
+    },
+  },
+  created() {
+    this.getProjetos()
+  }
+}
+</script>
