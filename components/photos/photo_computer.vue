8c7706366da3508b3d4082de13fca21f135e7b7d
Eduarda Ramos <2171347@my.ipleiria.pt>
2020-12-15 23:50:41 +0000

    Adicionados ficheiros.

---
+++ components/photos/photo_computer.vue
@@ -0,0 +1,18 @@
+<template>
+  <img
+    class="VuetifyLogo"
+    alt="Vuetify Logo"
+    src="../../static/Image_Search.jpg"
+  >
+</template>
+
+<style>
+.VuetifyLogo {
+/*
+  height: 480px;
+  width: 480px;
+*/
+
+}
+
+</style>
